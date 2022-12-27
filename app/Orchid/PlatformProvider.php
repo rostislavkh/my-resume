634665d305b4b4b7bc3b5f8baf64b4bb1da1de89
Rostik <rostislav.kh@zina.com.ua>
2022-12-27 17:58:38 +0200

    added admin panel

---
+++ app/Orchid/PlatformProvider.php
@@ -0,0 +1,62 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Orchid;
+
+use Orchid\Platform\Dashboard;
+use Orchid\Platform\ItemPermission;
+use Orchid\Platform\OrchidServiceProvider;
+use Orchid\Screen\Actions\Menu;
+use Orchid\Support\Color;
+
+class PlatformProvider extends OrchidServiceProvider
+{
+    /**
+     * @param Dashboard $dashboard
+     */
+    public function boot(Dashboard $dashboard): void
+    {
+        parent::boot($dashboard);
+
+        // ...
+    }
+
+    /**
+     * @return Menu[]
+     */
+    public function registerMainMenu(): array
+    {
+        return [
+            Menu::make(__('Users'))
+                ->icon('user')
+                ->route('platform.systems.users')
+                ->permission('platform.systems.users')
+                ->title(__('Access rights')),
+        ];
+    }
+
+    /**
+     * @return Menu[]
+     */
+    public function registerProfileMenu(): array
+    {
+        return [
+            Menu::make(__('Profile'))
+                ->route('platform.profile')
+                ->icon('user'),
+        ];
+    }
+
+    /**
+     * @return ItemPermission[]
+     */
+    public function registerPermissions(): array
+    {
+        return [
+            ItemPermission::group(__('System'))
+                ->addPermission('platform.systems.roles', __('Roles'))
+                ->addPermission('platform.systems.users', __('Users')),
+        ];
+    }
+}
---
+++ app/Orchid/Screens/PlatformScreen.php
@@ -0,0 +1,74 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Orchid\Screens;
+
+use Orchid\Screen\Actions\Link;
+use Orchid\Screen\Screen;
+use Orchid\Support\Facades\Layout;
+
+class PlatformScreen extends Screen
+{
+    /**
+     * Fetch data to be displayed on the screen.
+     *
+     * @return array
+     */
+    public function query(): iterable
+    {
+        return [];
+    }
+
+    /**
+     * The name of the screen displayed in the header.
+     *
+     * @return string|null
+     */
+    public function name(): ?string
+    {
+        return 'Get Started';
+    }
+
+    /**
+     * Display header description.
+     *
+     * @return string|null
+     */
+    public function description(): ?string
+    {
+        return 'Welcome to your admin panel.';
+    }
+
+    /**
+     * The screen's action buttons.
+     *
+     * @return \Orchid\Screen\Action[]
+     */
+    public function commandBar(): iterable
+    {
+        return [
+            Link::make('Website')
+                ->href(env('APP_URL', '/'))
+                ->icon('globe-alt')
+                ->target('_blank'),
+
+            Link::make('GitHub')
+                ->href('https://github.com/rostislavkh/my-resume')
+                ->icon('social-github')
+                ->target('_blank'),
+        ];
+    }
+
+    /**
+     * The screen's layout elements.
+     *
+     * @return \Orchid\Screen\Layout[]
+     */
+    public function layout(): iterable
+    {
+        return [
+            
+        ];
+    }
+}
